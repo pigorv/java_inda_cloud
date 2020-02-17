195d262a704b4a164af00efd9f65043bb1a0dffa
Ihor Prysiazhnyi <igor.prisyazhnyi@gmail.com>
2020-02-17 18:18:34 +0200

    Containerize catalog/order modules

--- pc/src/main/java/org/nipu/pc/PcApplication.java
+++ pc/src/main/java/org/nipu/pc/PcApplication.java
@@ -1,14 +1,17 @@
 package org.nipu.pc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//@Slf4j
 public class PcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PcApplication.class, args);
+//        log.info("Application started.");
     }
 }
