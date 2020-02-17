0007b164b7081e6aa2b90d6e9c4fa05acd2acb7f
Ihor Prysiazhnyi <igor.prisyazhnyi@gmail.com>
2020-02-17 23:09:00 +0200

    Add ELK services for centralized logging

--- pc/src/main/java/org/nipu/pc/PcApplication.java
+++ pc/src/main/java/org/nipu/pc/PcApplication.java
@@ -1,17 +1,21 @@
 package org.nipu.pc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@Slf4j
+@Slf4j
 public class PcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PcApplication.class, args);
-//        log.info("Application started.");
+        log.info("Catalog application have been started.");
     }
 }
--- po/src/main/java/org/nipu/po/PoApplication.java
+++ po/src/main/java/org/nipu/po/PoApplication.java
@@ -1,5 +1,6 @@
 package org.nipu.po;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,9 +9,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@Slf4j
 public class PoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PoApplication.class, args);
+        log.info("Order application have been started.");
     }
 }
